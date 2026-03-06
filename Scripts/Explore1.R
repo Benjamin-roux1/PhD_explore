0aeef0f6dfb3ebe17464754b481d6dcd9de2fe6e
Benjamin Paul Marie Roux <berou1714@nmbu.no>
2026-03-06 16:30:00 +0100

    Maj

--- Scripts/Explore1.R
+++ Scripts/Explore1.R
@@ -1,5 +1,3 @@
-setwd("C:/Users/berou1714/OneDrive - Norwegian University of Life Sciences/Desktop/PhD-explore")
-
 install.packages("terra")
 install.packages("sf")
 install.packages("leaflet")
@@ -12,7 +10,7 @@ GMBA <- st_read("GMBA_inventory_v2.0_standard_300/GMBA_inventory_v2.0_standard_3
 mapview(GMBA, zcol = "MapName", legend = FALSE)
 
 GMBA_vect <- terra::vect(GMBA)
-plot(GMBA_vect) #ej fais une modification ici 
+plot(GMBA_vect) 
 
 CHELSA_clim <- read.table("CHELSA/CHELSA_clim.txt")[,1]
 NAME <- unlist(lapply(strsplit(CHELSA_clim,"/"),function(x) x[10]))
@@ -21,10 +19,10 @@ for (i in 1:length(CHELSA_clim)){
   print(paste(i,length(CHELSA_clim),sep=" out of "))
   shell(cmd=paste('wget -nc -c -nd ',CHELSA_clim[i],sep=""))
   tmp <- terra::mask(terra::rast(NAME[i]),GMBA_vect)
-  terra::writeRaster(tmp_mask,file.path("CHELSA", paste0("GMBA_", NAME[i])),overwrite=T)
+  terra::writeRaster(tmp,file.path("CHELSA", paste0("GMBA_", NAME[i])),overwrite=T)
   file.remove(NAME[i])
 }
 
-#je m'apelle
-#oui oui
-#coebnjfdcneb
+tif_files <- list.files("CHELSA/", pattern = "\\.tif$", full.names = TRUE)
+rasters_stack <- terra::rast(tif_files)
+plot(rasters_stack)
