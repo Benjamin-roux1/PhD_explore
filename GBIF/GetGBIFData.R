ea2996c39677a4f0e91c63b3be7b372e2a957b30
Benjamin Paul Marie Roux <berou1714@nmbu.no>
2026-03-23 08:37:00 +0100

    Change of the spatial filter, from bbox to multipolygons. Added a temporal filter if necessary. Unzip only the occurrence.txt. Change the logic of the reading/writing part, by reading/writing in parquet year by year instead of reading the whole data frame and then writing in parquet.

--- GBIF/GetGBIFData.R
+++ GBIF/GetGBIFData.R
@@ -4,10 +4,11 @@ DownloadGBIF <- function(key, user, user.email, pwd, custom.shp) {
   if (is.null(custom.shp)) stop("A valid spatial object (sf/Spatial) is required.")
   
   # 1. Convert shape to WKT
-  aoi_wkt <- rgbif::gbif_bbox2wkt(bbox = sf::st_bbox(custom.shp))
+  aoi_wkt <- sf::st_as_text(custom.shp)
   
   # 2. Trigger download - CHANGE FORMAT TO "DWCA"
   req <- rgbif::occ_download(
+    # rgbif::pred_in("year", 1980), # Add only if the file is too big
     rgbif::pred_in("taxonKey", key),
     rgbif::pred("hasCoordinate", TRUE),
     rgbif::pred("hasGeospatialIssue", FALSE),
@@ -36,7 +37,7 @@ DownloadGBIF <- function(key, user, user.email, pwd, custom.shp) {
   
   # DwC-A contains multiple files, so we unzip into a folder named by the Key
   unzip_dir <- file.path("GBIF/data/dwc_raw", as.character(req))
-  unzip(download_info, exdir = unzip_dir)
+  unzip(download_info, files = "occurrence.txt", exdir = unzip_dir)
   
   # Remove the original zip file
   file.remove(download_info)
@@ -47,7 +48,6 @@ DownloadGBIF <- function(key, user, user.email, pwd, custom.shp) {
   return(list(
     key = as.character(req),
     file_path = occurrence_file,
-    all_files = list.files(unzip_dir, full.names = TRUE),
     citation = rgbif::gbif_citation(as.character(req))
   ))
 }
@@ -58,35 +58,36 @@ GBIF_to_Parquet <- function(file_path, parquet_dir) {
   
   # 1. Create output directory if needed
   if (!dir.exists(parquet_dir)) {
-    
     dir.create(parquet_dir, recursive = TRUE)
+  }
   
   message("Starting chunked read of GBIF CSV...")
   
-  # 2. Read + process in chunks
-  occs <- bigreadr::big_fread1(file_path, every_nlines = 1e6, integer64 = "double",
-    verbose = TRUE, .transform = function(x) {
-      message("Processing chunk of ", nrow(x), " rows...")
-      x %>%
+  chunk_id <- 0
+  
+  # 2. Read + process in chunks + write to Parquet
+  bigreadr::big_fread1(file_path, every_nlines = 1e6, integer64 = "double", 
+                               nThread = parallel::detectCores()-1, verbose = TRUE, 
+                               .transform = function(x) {
+                                 chunk_id <- chunk_id + 1
+      message("Processing chunk of ", chunk_id, " of ", nrow(x), " rows...")
+      processed <- x %>%
         dplyr::select(
           species, scientificName, kingdom, phylum, class, order, family, genus, 
           taxonRank, occurrenceID, basisOfRecord, eventDate, year, countryCode,
           decimalLatitude, decimalLongitude, coordinateUncertaintyInMeters
         ) %>%
         dplyr::distinct(species, decimalLongitude, decimalLatitude, eventDate, .keep_all = TRUE)
-    }
-  )
-  
-  # 3. Write to Parquet
-  message("Writing to Parquet...")
-  arrow::write_dataset(occs, path = parquet_dir, format = "parquet", partitioning = "year")
+      
+      arrow::write_dataset(processed, path = parquet_dir, format = "parquet", partitioning = "year",
+                           basename_template = paste0("chunk_", chunk_id, "_part{i}.parquet"))
+      
+      return(NULL)
+    })
   
   # 4. Cleanup 
   unlink(dirname(file_path), recursive = TRUE)
-  rm(occs)
   gc()
   
   message("Conversion to Parquet complete!")
-  
-  }
 }
